2850ffcdf954921b1f79dd83eb170ed1ee3deb23
Brad Gibbs <bradgibbs@mac.com>
2021-02-18 18:37:15 -0600

    stuff

--- src/components/CarouselLayout.tsx
+++ src/components/CarouselLayout.tsx
@@ -1,13 +1,12 @@
 import Head from "next/head";
 
-const SingleImageLayout: React.FC<{ title?: string }> = ({ children, title }) => {
+const SingleImageLayout: React.FC = ({ children }) => {
   if (typeof window === "undefined") {
     return null;
   }
   return (
     <>
       <Head>
-        <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
