3652e24bf212c5cb25a4e571adc5d21f2c313ad3
Brad Gibbs <bradgibbs@mac.com>
2021-02-20 11:06:07 -0600

    self-hosted Raleway font

--- src/gp-theme.ts
+++ src/gp-theme.ts
@@ -56,6 +56,7 @@ const gpTheme: ThemeConfig = {
           font-family: "Raleway";
           font-style: normal;
           font-weight: 400;
+          font-display: swap;
           src: local(""), url("../fonts/raleway-v19-latin-regular.woff2") format("woff2"),
             /* Chrome 26+, Opera 23+, Firefox 39+ */ url("../fonts/raleway-v19-latin-regular.woff")
               format("woff"); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
@@ -65,6 +66,7 @@ const gpTheme: ThemeConfig = {
           font-family: "Raleway";
           font-style: normal;
           font-weight: 700;
+          font-display: swap;
           src: local(""), url("../fonts/raleway-v19-latin-700.woff2") format("woff2"),
             /* Chrome 26+, Opera 23+, Firefox 39+ */ url("../fonts/raleway-v19-latin-700.woff")
               format("woff"); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
