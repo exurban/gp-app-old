ee3e09e0c2fec620daf8e79fbd527e92d90c1eb5
Brad Gibbs <bradgibbs@mac.com>
2021-02-21 16:34:18 -0600

    poop

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,12 +1,22 @@
 export default {
   openGraph: {
-    type: "website",
+    type: "grapes",
     locale: "en_US",
     url: "https://www.gibbs-photography.com",
-    site_name: "Dum-Dum"
+    site_name: "Dum-Dum",
+    title: "stupid, stupid, stupid!",
+    description: "poopy pants",
+    images: [
+      {
+        url:
+          "https://configcdkstack-gpbucketc7c11d3d-qtgzc43jqi2c.s3.us-east-2.amazonaws.com/photo_1043-1612553422228.webp",
+        width: 800,
+        height: 600
+      }
+    ]
   },
   twitter: {
-    handle: "@gibbs_photog",
+    handle: "@de-douche",
     site: "https://i-is-a-dum-dum.com",
     cardType: "summary_large_image"
   }
