ef8d13d0b6afee3627e520d8c1cdf92204875dfc
Brad Gibbs <bradgibbs@mac.com>
2021-02-22 16:19:56 -0600

    more stuff

--- next-seo.config.js
+++ next-seo.config.js
@@ -3,7 +3,9 @@ export default {
     type: "website",
     locale: "en_US",
     url: "https://www.gibbs-photography.com",
-    site_name: "Gibbs Photography"
+    site_name: "Gibbs Photography",
+    title: "Gibbs Photography - Default SEO",
+    description: "GP Description from Default SEO"
   },
   twitter: {
     handle: "@gibbs_photog",
