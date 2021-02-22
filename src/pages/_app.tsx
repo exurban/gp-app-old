0332bb991a8eec56a583df17075d398aac0da77d
Brad Gibbs <bradgibbs@mac.com>
2021-02-21 18:54:59 -0600

    f

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../lib/apolloClient";
@@ -31,7 +32,6 @@ const App = ({ Component, pageProps }: AppProps): JSX.Element => {
     <>
       <ApolloProvider client={apolloClient}>
         <AuthProvider session={pageProps.session}>
-          <DefaultSeo {...SEO} />
           <BumbagProvider isSSR colorMode="dark" theme={gpTheme}>
             {router.pathname.startsWith(`/carousel/`) ? (
               <CarouselLayout>
@@ -39,6 +39,35 @@ const App = ({ Component, pageProps }: AppProps): JSX.Element => {
               </CarouselLayout>
             ) : (
               <Layout>
+                <Head>
+                  <link
+                    rel="preload"
+                    href="/fonts/raleway-v19-latin-700.woff2"
+                    as="font"
+                    type="font/woff2"
+                    crossOrigin="anonymous"
+                  />
+                  <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+                  <link href="/static/favicons/site.webmanifest" rel="manifest" />
+                  <link
+                    href="/static/favicons/apple-touch-icon.png"
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                  />
+                  <link
+                    href="/static/favicons/favicon-32x32.png"
+                    rel="icon"
+                    sizes="32x32"
+                    type="image/png"
+                  />
+                  <link
+                    href="/static/favicons/favicon-16x16.png"
+                    rel="icon"
+                    sizes="16x16"
+                    type="image/png"
+                  />
+                </Head>
+                <DefaultSeo {...SEO} />
                 <Component {...pageProps} />
               </Layout>
             )}
