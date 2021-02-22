cf8fefec459d01cc73f2da92770852df9fc6a5a4
Brad Gibbs <bradgibbs@mac.com>
2021-02-22 16:37:44 -0600

    stuff

--- src/pages/gallery/photographer/[name].tsx
+++ src/pages/gallery/photographer/[name].tsx
@@ -1,3 +1,5 @@
+import { GetStaticProps, GetStaticPaths } from "next";
+import { addApolloState, initializeApollo } from "../../../lib/apolloClient";
 import { useRouter } from "next/router";
 import { useQuery } from "@apollo/client";
 import {
@@ -8,6 +10,7 @@ import Loader from "../../../components/Loader";
 import ErrorMessage from "../../../components/ErrorMessage";
 import React from "react";
 import SectionGallery from "../../../components/SectionGallery";
+import { NextSeo } from "next-seo";
 
 const PhotographerGallery: React.FC = () => {
   const router = useRouter();
@@ -34,14 +37,55 @@ const PhotographerGallery: React.FC = () => {
   const { photographerInfo, total, photos } = data.allPhotosByPhotographer;
 
   return (
-    <SectionGallery
-      coverImage={photographerInfo.coverImage}
-      name={photographerInfo.name}
-      description={photographerInfo.bio}
-      total={total}
-      photos={photos}
-    />
+    <>
+      <NextSeo
+        title="Photographer Gallery"
+        description="Description for photographer's gallery."
+        openGraph={{
+          images: [
+            {
+              url:
+                "https://configcdkstack-gpbucketc7c11d3d-qtgzc43jqi2c.s3.us-east-2.amazonaws.com/photo_1119-1612566116438.webp"
+            }
+          ]
+        }}
+      />
+      <SectionGallery
+        coverImage={photographerInfo.coverImage}
+        name={photographerInfo.name}
+        description={photographerInfo.bio}
+        total={total}
+        photos={photos}
+      />
+    </>
   );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { name: "Boyd Gibbs" } }, { params: { name: "Scott Gibbs" } }],
+    fallback: false
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  if (!params || typeof params.name !== "string") {
+    return;
+  }
+  const input = { name: params.name } as AllPhotosByPhotographerInput;
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query({
+    query: AllPhotosByPhotographerDocument,
+    variables: { input: input }
+  });
+
+  return addApolloState(apolloClient, {
+    props: {
+      initialApolloState: apolloClient.cache.extract()
+    },
+    revalidate: 1
+  });
 };
 
 export default PhotographerGallery;
