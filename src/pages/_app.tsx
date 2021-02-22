577917927ca3dc56abab60c0789d4daa74eb4cf2
Brad Gibbs <bradgibbs@mac.com>
2021-02-21 18:59:28 -0600

    kk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -32,6 +32,35 @@ const App = ({ Component, pageProps }: AppProps): JSX.Element => {
     <>
       <ApolloProvider client={apolloClient}>
         <AuthProvider session={pageProps.session}>
+          <Head>
+            <link
+              rel="preload"
+              href="/fonts/raleway-v19-latin-700.woff2"
+              as="font"
+              type="font/woff2"
+              crossOrigin="anonymous"
+            />
+            <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+            <link href="/static/favicons/site.webmanifest" rel="manifest" />
+            <link
+              href="/static/favicons/apple-touch-icon.png"
+              rel="apple-touch-icon"
+              sizes="180x180"
+            />
+            <link
+              href="/static/favicons/favicon-32x32.png"
+              rel="icon"
+              sizes="32x32"
+              type="image/png"
+            />
+            <link
+              href="/static/favicons/favicon-16x16.png"
+              rel="icon"
+              sizes="16x16"
+              type="image/png"
+            />
+          </Head>
+          <DefaultSeo {...SEO} />
           <BumbagProvider isSSR colorMode="dark" theme={gpTheme}>
             {router.pathname.startsWith(`/carousel/`) ? (
               <CarouselLayout>
@@ -39,35 +68,6 @@ const App = ({ Component, pageProps }: AppProps): JSX.Element => {
               </CarouselLayout>
             ) : (
               <Layout>
-                <Head>
-                  <link
-                    rel="preload"
-                    href="/fonts/raleway-v19-latin-700.woff2"
-                    as="font"
-                    type="font/woff2"
-                    crossOrigin="anonymous"
-                  />
-                  <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
-                  <link href="/static/favicons/site.webmanifest" rel="manifest" />
-                  <link
-                    href="/static/favicons/apple-touch-icon.png"
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                  />
-                  <link
-                    href="/static/favicons/favicon-32x32.png"
-                    rel="icon"
-                    sizes="32x32"
-                    type="image/png"
-                  />
-                  <link
-                    href="/static/favicons/favicon-16x16.png"
-                    rel="icon"
-                    sizes="16x16"
-                    type="image/png"
-                  />
-                </Head>
-                <DefaultSeo {...SEO} />
                 <Component {...pageProps} />
               </Layout>
             )}
