bf6966a453a46ff8616811d428b881ac3fa17ae3
Brad Gibbs <bradgibbs@mac.com>
2021-04-01 17:12:12 -0500

    th

--- src/pages/checkout/cancel.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from "next";
-import { Heading } from "bumbag";
-
-const CheckoutCanceled: NextPage = () => {
-  return <Heading use="h3">Stripe Payment Canceled!!!</Heading>;
-};
-
-export default CheckoutCanceled;
