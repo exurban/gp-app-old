8a68629f4eef2df261228979e320bc417cbb4b89
Brad Gibbs <bradgibbs@mac.com>
2021-04-03 15:18:27 -0500

    reset env variables

--- src/pages/carousel/photographer/[name].tsx
+++ src/pages/carousel/photographer/[name].tsx
@@ -29,9 +29,7 @@ const PhotographerCarousel: React.FC = () => {
     if (!photos || !activeIndex) {
       return;
     }
-    console.log(
-      `useEffect happened. selected photo at index: ${activeIndex}: ${photos[activeIndex].sku}`
-    );
+
     setActivePhoto(photos[activeIndex]);
   }, [activeIndex, setActiveIndex]);
 
